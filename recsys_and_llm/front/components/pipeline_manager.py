70799e09be23e69b7a3753725d898928cd3baa60
JiHwan <mobush99@gmail.com>
2025-03-15 17:33:38 +0900

    chore: Adjustment in CUSTOM_PROMPT

--- recsys_and_llm/front/components/pipeline_manager.py
+++ recsys_and_llm/front/components/pipeline_manager.py
@@ -18,7 +18,7 @@ custom_prompt = {
     "content": """
     You are a system that recommends movies to users.
     You must answer in English,
-    In the answer, the output format of Entity is
+    In the answer, the output format of Entity MUST be
     **Limited to the movie title (opening year). **
     **However, exceptions are made when users ask for recommendation reasons or information about recommended movies.**
     If you lack information about your preferences (actors, directors, genres), etc., you can continue the conversation as much as you need.
@@ -60,5 +60,5 @@ def set_pipeline(flag, rec_module=gpt_rec, gen_module=gpt_gen):
     flag, rec_module, gen_module에 따라 적절한 Pipeline 객체 생성.
     """
     return ChatgptAgent(
-        rec_module=gpt_rec, gen_module=gpt_gen, config=chat_gpt_agent_config
+        rec_module=gpt_rec, gen_module=gpt_gen_expansion, config=chat_gpt_agent_config
     )
